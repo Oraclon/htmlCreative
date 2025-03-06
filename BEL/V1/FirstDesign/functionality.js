9d64d61f4164e862ed3044ec705db8a070ea1082
GTrouble <giorgosbls1@gmail.com>
2025-03-06 23:10:07 +0200

    V1

---
+++ BEL/V1/FirstDesign/functionality.js
@@ -0,0 +1,6 @@
+const CreateDomElement = (tag, css, text) => {
+    let element = document.createElement(tag);
+    element.className = css;
+    if (text != undefined) element.innerHTML = text;
+    return element;
+};
