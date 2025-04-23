80f310ac7e5bf97823de8b9ec9186d1bc8c431e6
Raphael aka theScripter <73601567+Oraclon@users.noreply.github.com>
2025-04-23 13:37:50 +0300

    Create ProgramCSCors.cs

---
+++ canvas/ProgramCSCors.cs
@@ -0,0 +1,11 @@
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+app.UseCors("AllowAngular");
