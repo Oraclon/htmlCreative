bb1858fc6bc61a7a577caff8d3a49fec98fc08e6
Raphael aka theScripter <73601567+Oraclon@users.noreply.github.com>
2025-04-30 17:32:42 +0300

    Update block.cs

--- block.cs
+++ block.cs
@@ -33,11 +33,22 @@ namespace CNN.LayerLIB
         public int KernelSize           { get; set; }
         public int Stride               { get; set; }
         public int Padding              { get; set; }
+        private int _TrainingSampleSize     { get; set; }
         public Activation Activation    { get; set; }
         public double[][][] Activations { get; set; }
         public CNNFilter[] Kernels { get; set; }
         #endregion
-
+        private void _UpdateLayerActivations()
+        {
+            Activations = new double[_TrainingSampleSize][][];
+            for(int sample = 0; sample < _TrainingSampleSize; sample++)
+            {
+                double[][] kernelSamplesCollection = new double[NumberOfKernels][];
+                for(int kernel = 0; kernel < NumberOfKernels; kernel++)
+                    kernelSamplesCollection[kernel] = Kernels[kernel].Activations[sample];
+                Activations[sample] = kernelSamplesCollection;
+            }
+        }
         public void Train(double[][] inputs)
         {
             for(int kernel = 0; kernel < NumberOfKernels; kernel++)
@@ -48,18 +59,12 @@ namespace CNN.LayerLIB
         }
         public void Train(double[][][] inputs)
         {
+            _TrainingSampleSize = inputs.Length;
+
             for(int kernel = 0; kernel < NumberOfKernels; kernel++)
                 Kernels[kernel].Train(inputs);
 
-            double[][][] activations = new double[inputs.Length][][];
-            for(int sample = 0; sample < inputs.Length; sample++)
-            {
-                double[][] collection = new double[NumberOfKernels][];
-                for(int kernel = 0; kernel < NumberOfKernels; kernel++)
-                    collection[kernel] = Kernels[kernel].Activations[sample];
-                activations[sample] = collection; 
-            }
-            Activations = activations;
+            _UpdateLayerActivations();
         }
     }
 
